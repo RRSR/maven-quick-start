be28067a9ec8a3b776cf060d275641d2fb683795
Raj Rathore <Raj.Rathore@tatacommunications.com>
2018-03-05 10:00:56 +0530

    Added and used StringUtils dependency

--- src/main/java/raj/programming/training/Application.java
+++ src/main/java/raj/programming/training/Application.java
@@ -1,14 +1,23 @@
 package raj.programming.training;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Application {
     
     public Application() {
         System.out.println ("Inside Application");
     }
+    
+    public int countWords(String words){
+        String[] seperateWords = StringUtils.split(words, ' ');
+        return (seperateWords == null) ? 0 : seperateWords.length;
+    }
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
-    	System.out.println ("Starting Application");
-	Application app = new Application();
+        System.out.println ("Starting Application");
+        Application app = new Application();
+        int count = app.countWords("These are four words");
+        System.out.println("Word Count : "+count);
     }
 }
